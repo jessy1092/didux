4b2c3b268fb2cf60b49d9435e1d9e302be0cde4a
Lee <jessy1092@gmail.com>
2019-08-07 23:41:22 +0800

    feat: Support promise action

--- src/decorator.ts
+++ src/decorator.ts
@@ -3,6 +3,14 @@
 import Didux from './didux';
 import { ACTIONS_SYMBOL } from './constant';
 
+function isPromise(value: any) {
+	if (value !== null && typeof value === 'object') {
+		return value && typeof value.then === 'function';
+	}
+
+	return false;
+}
+
 export function action<T>(): MethodDecorator {
 	return (target, key, descriptor: PropertyDescriptor): PropertyDescriptor => {
 		const actionName = `@didux/${target.constructor.name}_${String(key)}`;
@@ -12,8 +20,16 @@ export function action<T>(): MethodDecorator {
 
 		const fn = descriptor.value;
 
-		descriptor.value = function WraFunc(...props: any[]): T {
+		descriptor.value = function WraFunc(...props: any[]): T | Promise<T> {
 			const result = fn.apply(this, props);
+
+			if (isPromise(result)) {
+				return result.then((data: T) => {
+					(this as Didux<T>).dispatch({ type: actionName, payload: data });
+
+					return data;
+				});
+			}
 
 			(this as Didux<T>).dispatch({ type: actionName, payload: result });
 
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ interface data {
 	count: number;
 }
 
+export const sleep = (time: number) => new Promise(resolve => setTimeout(() => resolve(), time));
+
 class Calculator extends Didux<data> {
 	setupDefaultState() {
 		return { count: 0 };
@@ -30,26 +32,41 @@ class Calculator extends Didux<data> {
 
 		return { count: state.count + num };
 	}
+
+	@action<data>()
+	async delayAdd(num: number, time: number): Promise<data> {
+		const state = this.getState();
+
+		await sleep(time);
+
+		return { count: state.count + num };
+	}
 }
 
-const calculator = new Calculator();
+async function exec() {
+	const calculator = new Calculator();
 
-calculator.subscribe(() => {
-	console.log('listener', calculator.getState());
-});
+	calculator.subscribe(() => {
+		console.log('listener', calculator.getState());
+	});
 
-console.log(calculator.getState());
+	console.log(calculator.getState());
 
-console.log(calculator.addOne());
+	console.log('Get add one', calculator.addOne());
 
-console.log('result', calculator.getState());
+	console.log('result', calculator.getState());
 
-console.log(calculator.add(10));
+	console.log('add 10?', calculator.add(10));
 
-console.log('add 10?', calculator.getState());
+	console.log('reset');
 
-console.log('reset');
+	console.log('reset?', calculator.reset());
 
-calculator.reset();
+	const result = await Promise.all([calculator.delayAdd(1, 5000), calculator.delayAdd(10, 1000)]);
+
+	console.log(result);
+}
+
+exec();
 
 export default Calculator;
